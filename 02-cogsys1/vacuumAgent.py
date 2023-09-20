092a9f5cc363359f9029f591442233da58dc8735
erosec4 <emmac4164@gmail.com>
2023-09-20 14:08:13 -0400

    Vacuum path corrected

--- 02-cogsys1/vacuumAgent.py
+++ 02-cogsys1/vacuumAgent.py
@@ -17,26 +17,27 @@ class VacuumAgent(python_actr.ACTR):
 	motorInst = MotorModule()
 	cleanSensor = CleanSensorModule()
 
-	def init():
-		goal.set("rsearch left 1 0 1")
+	def init(): # Initialize agent
+		goal.set("rsearch left 1 0 1") # Initial short-term memory
 		self.home = None
 
 	#----ROOMBA----#
 
-	def clean_cell(cleanSensor="dirty:True", utility=0.6):
+	def clean_cell(cleanSensor="dirty:True", motorInst="busy:False", utility=0.6):
 		motorInst.clean()
 
 	def forward_rsearch(goal="rsearch left ?dist ?num_turns ?curr_dist",
-						motorInst="busy:False", body="ahead_cell.wall:False"):
+						motorInst="busy:False", body="ahead_cell.wall:False"): # Check if goal buffer has slot values of "rsearch' and "left"
 		motorInst.go_forward()
 		print(body.ahead_cell.wall)
-		curr_dist = str(int(curr_dist) - 1)
+		curr_dist = str(int(curr_dist) - 1) # curr_dist var created from "?curr_dist" slot in the condition; change to adjust goal state
 		goal.set("rsearch left ?dist ?num_turns ?curr_dist")
 
 	def left_rsearch(goal="rsearch left ?dist ?num_turns 0", motorInst="busy:False",
 					utility=0.1):
 		motorInst.turn_left(2)
 		num_turns = str(int(num_turns) + 1)
+		dist = str(int(dist) + 1) # NEW wait it actually worked
 		goal.set("rsearch left ?dist ?num_turns ?dist")
 
 
