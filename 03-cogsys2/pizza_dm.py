8e6935ccff450070eb1777647b262d9da4ab6300
erosec4 <emmac4164@gmail.com>
2023-10-03 15:16:59 -0400

    Goal slot/values added, HDM changed to just Memory to stop infinite loop

--- 03-cogsys2/pizza_dm.py
+++ 03-cogsys2/pizza_dm.py
@@ -8,7 +8,7 @@ from python_actr.actr.hdm import *
 class PizzaBuilder_DM(ACTR):
     goal = Buffer()
     retrieval = Buffer()
-    DM_module = HDM(retrieval,finst_size=22,finst_time=100.0) # Added finst_size=22,finst_time=100.0 parameters
+    DM_module = Memory(retrieval)
     my_pizza = []
 
     def cook_pizza(self, pizza_ingred):
@@ -36,18 +36,19 @@ class PizzaBuilder_DM(ACTR):
 
     def prep_ingredients(goal="start_pizza"):
         # Start building our pizza!
-        goal.set("build_pizza")
+        goal.set("build_pizza done:prep new:any")
         # Request next step from DM
         DM_module.request("prev:prep next:?next_ingred") # Need retrieval
         
     ###Rules to request from declarative memory for next step/ingredient and place that ingredient on your pizza and make sure you can more on to cooking pizza
-    def add_toppings(goal="build_pizza", retrieval="prev:?x next:?next_ingred!onion"): # Slot name != value
+    def add_toppings(goal="build_pizza done:?a new:?b", retrieval="prev:?x next:?next_ingred!onion"): # Slot name != value
         my_pizza.append(next_ingred)
         # Request next step from DM
         DM_module.request("prev:?next_ingred next:?y")
+        goal.modify(done=next_ingred) # Take y from memory request
     
     # Last step: onion, then cook
-    def add_onion(goal="build_pizza", retrieval="prev:?x next:onion"):
+    def add_onion(goal="build_pizza done:?a new:?b", retrieval="prev:?x next:onion"):
         my_pizza.append("onion")
         goal.set("cook_pizza")
 
